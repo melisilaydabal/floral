fc43be4b56fff8694f5ebe8d4bba869c1aee5826
Melis Ilayda Bal <63544317+melisilaydabal@users.noreply.github.com>
2025-07-14 10:19:47 +0200

    Update scripts

--- scripts/update_batch_size_configs.sh
+++ scripts/update_batch_size_configs.sh
@@ -4,7 +4,6 @@ read -p "Enter the folder path containing the .yaml files: " folder_path
 
 read -p "Enter the value you want to set for 'batch_size': " new_value
 
-# Iterate over each .yaml file in the folder
 for file in "$folder_path"/*.yaml; do
     sed -i "s/^\(\s*batch_size:\s*\).*/\1 $new_value/" "$file"
 done
