0af1848dca0f762a6539e4a92014b41e12e74a77
Melis Ilayda Bal <63544317+melisilaydabal@users.noreply.github.com>
2025-07-14 10:18:44 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,6 @@
 
 repo_path=$~/floral    # adjust this after you clone the repository in your local machine
 
-#source /home/fast/mbal/robust_label_venv/bin/activate
-#pip freeze > requirements.txt
-
-
 virtualenv -p python3 ./floral_venv  # path to venv
 
 pip install -r ./requirements.txt
